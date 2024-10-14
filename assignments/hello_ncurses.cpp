4bc7c60b15d1f5c1f44e44968afacfa4cfa6c250
aaniaahh <aamyles1222@my.msutexas.edu>
2024-10-14 16:27:20 -0500

    added P02A

--- assignments/hello_ncurses.cpp
+++ assignments/hello_ncurses.cpp
@@ -2,6 +2,7 @@
 
 int main() {
     initscr();            // Start ncurses mode
+    box(stdscr, 0, 0);
     printw("Hello, World!"); // Print message on the screen
     refresh();            // Refresh to show the message
     getch();              // Wait for user input
